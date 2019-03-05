ce02ecae7f7254c8e8442e62c82b591fffe94199
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-03-05 11:30:42 +0100

    added gem to handle money in rails

--- app/controllers/teddies_controller.rb
+++ app/controllers/teddies_controller.rb
@@ -1,2 +1,15 @@
 class TeddiesController < ApplicationController
+  skip_before_action :authenticate_user!
+  #hacemos esto para que en todos los methods que tengamos no sea necesario
+  #que el usuario este registrado
+
+  def index
+    @teddies = Teddy.all
+
+  end
+
+  def show
+    @teddy = Teddy.find(params[:id])
+
+  end
 end
