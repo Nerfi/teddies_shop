73cb93826dfcd151f3e8f233beb3ff7b3efb2501
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-03-06 12:03:35 +0100

    working stripe

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,4 +7,10 @@ def create
   redirect_to new_order_payment_path(order)
 
  end
+
+ def show
+   @order = current_user.orders.where(state: 'paid').find(params[:id])
+
+
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'teddies#index'
   resources :teddies, only: [:index, :show]
   resources :orders, only: [:show, :create] do
-    resources :payment, only: [:new, :create]
+    resources :payments, only: [:new, :create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
