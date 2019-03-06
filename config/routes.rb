9060e4faf8204588af5c55e01c86d87d01a8c07a
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-03-06 10:22:15 +0100

    added a new routes t hte orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'teddies#index'
-  resources :teddies, only:[:index, :show]
+  resources :teddies, only: [:index, :show]
+  resources :orders, only: [:show, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
